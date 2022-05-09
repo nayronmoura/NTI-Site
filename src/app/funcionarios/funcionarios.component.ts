49b01c925e18da2caea2be2c3e7def5c00c137d8
Nayron Moura <nayronmoura@gmail.com>
2022-05-09 16:24:40 -0300

    bootstrap adicionado

--- src/app/funcionarios/funcionarios.component.ts
+++ src/app/funcionarios/funcionarios.component.ts
@@ -6,16 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./funcionarios.component.css']
 })
 export class FuncionariosComponent {
+  //bootstrap: https://getbootstrap.com/docs/5.1/layout/breakpoints/
+  //deixar o site responsivo com o bootstrap.
+  //melhorar o design dos funcionários e jogar os nomes para dentro da div principal
+  //mudar as fontes
+  //melhorar as animações das imagens e melhorar o gif
+  //melhorar o alinhamento do as informações
+  //adicionar os links das imagens dos serviçoes desenvolvidos
+  //
   URLimages;
   constructor(){
     this.URLimages=[
-      ['http://ntist.com.br/static/controle_site/images/Junior.png', 'Junior'],
-      ['http://ntist.com.br/static/controle_site/images/Douglas.png', 'Douglas'],
-      ['http://ntist.com.br/static/controle_site/images/JV.png','João Victor'],
-      ['http://ntist.com.br/static/controle_site/images/Bruno.png','Bruno'],
-      ['http://ntist.com.br/static/controle_site/images/Lucas.png','Lucas'],
-      ['http://ntist.com.br/static/controle_site/images/Carlos.png','Carlos'],
-      ['http://ntist.com.br/static/controle_site/images/marcilio.png','Marcilio']
+      ['http://ntist.com.br/static/controle_site/images/Junior.png', 'Junior',"adds"],
+      ['http://ntist.com.br/static/controle_site/images/Douglas.png', 'Douglas',"adds"],
+      ['http://ntist.com.br/static/controle_site/images/JV.png','João Victor',"adds"],
+      ['http://ntist.com.br/static/controle_site/images/Bruno.png','Bruno',"adds"],
+      ['http://ntist.com.br/static/controle_site/images/Lucas.png','Lucas',"adds"],
+      ['http://ntist.com.br/static/controle_site/images/Carlos.png','Carlos',"adds"],
+      ['http://ntist.com.br/static/controle_site/images/marcilio.png','Marcilio',"adds"]
     ]
   }
 }
---
+++ src/app/menu-mobile/menu-mobile.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuMobileComponent } from './menu-mobile.component';
+
+
+
+@NgModule({
+  declarations: [
+    MenuMobileComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MenuMobileModule { }
